a0219f8746dd246a102e7912464b4e12115c7b66
Maarten Berkenbosch <maarten@beebase.nl>
2015-10-19 02:54:20 +0200

    re arrange file structure

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+/*
+ var express = require('express');
+ var bodyParser = require('body-parser');
+ var morgan = require('morgan');
+ var http = require('http');
+
+ var app = express();
+ var publicRoot = __dirname + '/public';
+ app.use(express.static(publicRoot));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: true}));
+ app.use(morgan('dev'));
+
+ app.get('/test', function(req, res) {
+ res.send('test is working');
+ });
+ app.all('/', function(req, res, next) {
+ res.sendFile('index.html', {root: publicRoot});
+ });
+
+ http.createServer(app).listen(4000);
+ */
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var docRoot = __dirname + '/public';
 var http = require('http');
 
 var app = express();
-var publicRoot = __dirname + '/public';
-app.use(express.static(publicRoot));
+app.use(express.static(docRoot));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
@@ -13,8 +37,9 @@ app.use(morgan('dev'));
 app.get('/test', function(req, res) {
   res.send('test is working');
 });
-app.all('/', function(req, res, next) {
-  res.sendFile('index.html', {root: publicRoot});
+
+app.all('/*', function(req, res) {
+  res.sendFile('index.html', {root: docRoot});
 });
 
-http.createServer(app).listen(4000);
+http.createServer(app).listen(4444);
